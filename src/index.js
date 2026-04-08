c865f90d06853a2543636bd10bb0d79d89e002f2
avi1703 <avinashsharma301997@gmail.com>
2026-04-08 22:27:56 +0530

    Fixed Few Bugs

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from "react";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="71640961818-8k1nk0u74bmdfm4dunt09sf38atvd784.apps.googleusercontent.com">
-        <App />
+    <GoogleOAuthProvider
+      clientId="
+928269615201-92djec95aqi3ontuotc9715uv4uqh981.apps.googleusercontent.com"
+    >
+      <App />
     </GoogleOAuthProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
