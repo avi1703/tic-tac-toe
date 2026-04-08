c7c9fb362122038b876d80a4953b393e6e9e8b08
avi1703 <avinashsharma301997@gmail.com>
2026-04-08 22:30:22 +0530

    Fixed Few Bugs

--- src/index.js
+++ src/index.js
@@ -8,8 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <GoogleOAuthProvider
-      clientId="
-928269615201-92djec95aqi3ontuotc9715uv4uqh981.apps.googleusercontent.com"
+      clientId="928269615201-92djec95aqi3ontuotc9715uv4uqh981.apps.googleusercontent.com"
     >
       <App />
     </GoogleOAuthProvider>
